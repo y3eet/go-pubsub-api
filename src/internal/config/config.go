d7e22d9f5e3326c779863d1953f03ffe4127bb92
Jc <107809331+y3eet@users.noreply.github.com>
2026-03-27 09:55:11 +0800

    feat: update config

--- src/internal/config/config.go
+++ src/internal/config/config.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Config struct {
-	Port        string
-	JWTSecret   string
-	AppEnv      string
-	FrontendURL string
+	Port            string
+	JWTSecret       string
+	AppEnv          string
+	AuthCallbackURL string
 }
 
 var Cfg *Config
@@ -25,10 +25,10 @@ func Load() *Config {
 	}
 
 	Cfg = &Config{
-		Port:        getEnv("PORT", "8080"),
-		JWTSecret:   getEnv("JWT_SECRET_KEY", "defaultsecret"),
-		AppEnv:      getEnv("APP_ENV", "local"),
-		FrontendURL: getEnv("FRONTEND_URL", "http://localhost:3000"),
+		Port:            getEnv("PORT", "8080"),
+		JWTSecret:       getEnv("JWT_SECRET_KEY", "defaultsecret"),
+		AppEnv:          getEnv("APP_ENV", "local"),
+		AuthCallbackURL: getEnv("AUTH_CALLBACK_URL", "http://localhost:8080/auth/callback"),
 	}
 	return Cfg
 }
